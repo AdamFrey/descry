83af047c11c151ae153f81c140d130a23d37f59f
Adam Frey <adam@adamfrey.me>
2017-01-31 23:41:46 -0500

    Add styling to table

--- src/descry/ui/core.cljs
+++ src/descry/ui/core.cljs
@@ -10,25 +10,31 @@
 (rum/defc entity-table
   < rum/static
   [entities]
-  (let [entities (into {}
-                   (map (fn [[key val]]
-                          [key (attrs->entity-map val)]) entities))
-        headers  (reduce
-                   (fn [acc [_ e-map]]
-                     (apply conj acc (keys e-map)))
-                   #{}
-                   entities)
+  (let [entities       (into {}
+                         (map (fn [[key val]]
+                                [key (attrs->entity-map val)]) entities))
+        headers        (reduce
+                         (fn [acc [_ e-map]]
+                           (apply conj acc (keys e-map)))
+                         #{}
+                         entities)
         sorted-headers (sort headers)]
-    [:table
-     [:tr
-      [:th "id"]
-      (for [header sorted-headers]
-        [:th (str header)])]
-     (for [[id entity] entities]
-       [:tr
-        [:td id]
-        (for [header sorted-headers]
-          [:td (str (get entity header))])])]))
+    [:table {:style {:border-collapse "collapse"
+                     :font-family     "Lucida Sans Unicode, Lucida Grande, Sans-Serif"
+                     :width           "100%"
+                     :text-align      "left"}}
+     [:thead {:style {:border-bottom "2px solid #000"}}
+      [:tr
+       [:th "id"]
+       (for [header sorted-headers]
+         [:th {:key header} (str header)])]]
+     [:tbody
+      (for [[id entity] entities]
+        [:tr {:key   id
+              :style {:border-bottom  "1px solid black"}}
+         [:td {:style {:padding "10px"}} id]
+         (for [header sorted-headers]
+           [:td {:key header} (str (get entity header))])])]]))
 
 (defn mount [element source]
   (rum/mount (entity-table (ds/all-entities source)) element))
